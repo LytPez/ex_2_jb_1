e2ddd6c686cbc23044e7c98372ae752001aa4d2d
Guilherme <guilhermeisk@hotmail.com>
2022-08-04 11:42:20 -0300

    Exercicio 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function numberRandom(){
+//   “Função criada em um ficheiro externo que mostra uma
+// mensagem
+  let numeros=Math.floor(Math.random() * 50 + 1)
+
+  let aleatorios=document.getElementById('random').innerText=numeros 
+
+
+}
